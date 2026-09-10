b9397bac98b5a17db9a79b8c43fcc705562cd820
Angshuman Bhagat <angshumanbhagat414@gmail.com>
2026-09-10 10:42:25 +0530

    Time: 3 ms (76.62%) | Memory: 46.3 MB (74.09%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int romanToInt(String s) {
+        int r = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int a = v(s.charAt(i));
+
+            if (i < s.length() - 1 && a < v(s.charAt(i + 1)))
+                r -= a;
+            else
+                r += a;
+        }
+
+        return r;
+    }
+
+    int v(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+}
