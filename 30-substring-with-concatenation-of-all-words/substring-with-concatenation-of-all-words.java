3f5b0ce57d0a4026f5cdca611d7cf20ca3521c76
Angshuman Bhagat <angshumanbhagat414@gmail.com>
2026-09-16 09:01:16 +0530

    Time: 12 ms (49.15%) | Memory: 47.5 MB (23.72%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -0,0 +1,55 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] w) {
+        List<Integer> ans = new ArrayList<>();
+        if (s.length() == 0 || w.length == 0) return ans;
+
+        int len = w[0].length();
+        int cnt = w.length;
+        int total = len * cnt;
+
+        if (s.length() < total) return ans;
+
+        Map<String, Integer> need = new HashMap<>();
+
+        for (String x : w) {
+            need.put(x, need.getOrDefault(x, 0) + 1);
+        }
+
+        for (int st = 0; st < len; st++) {
+            Map<String, Integer> have = new HashMap<>();
+            int left = st, used = 0;
+
+            for (int right = st; right + len <= s.length(); right += len) {
+                String x = s.substring(right, right + len);
+
+                if (!need.containsKey(x)) {
+                    have.clear();
+                    used = 0;
+                    left = right + len;
+                    continue;
+                }
+
+                have.put(x, have.getOrDefault(x, 0) + 1);
+                used++;
+
+                while (have.get(x) > need.get(x)) {
+                    String y = s.substring(left, left + len);
+                    have.put(y, have.get(y) - 1);
+                    left += len;
+                    used--;
+                }
+
+                if (used == cnt) {
+                    ans.add(left);
+
+                    String y = s.substring(left, left + len);
+                    have.put(y, have.get(y) - 1);
+                    left += len;
+                    used--;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
