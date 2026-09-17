a48c7cf200f545532f107d5dafbf7d04c1664db9
Angshuman Bhagat <angshumanbhagat414@gmail.com>
2026-09-17 23:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (15.86%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void nextPermutation(int[] a) {
+        int n = a.length, i = n - 2;
+        while (i >= 0 && a[i] >= a[i + 1]) i--;
+        if (i >= 0) {
+            int j = n - 1;
+            while (a[j] <= a[i]) j--;
+            s(a, i, j);
+        }
+        r(a, i + 1, n - 1);
+    }
+
+    private void s(int[] a, int i, int j) {
+        int t = a[i]; a[i] = a[j]; a[j] = t;
+    }
+
+    private void r(int[] a, int l, int r) {
+        while (l < r) s(a, l++, r--);
+    }
+}
