ae369b57b57abada411a31c1177c92f9db114617
Angshuman Bhagat <angshumanbhagat414@gmail.com>
2026-09-18 12:08:25 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (82.94%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,3 +1,5 @@
+//anxshh
+//i am taking the approach where a binary search is conducted on the sorted array to find the exact target undex or the position where it would be inserted to maintain the order in O(log n) time.
 class Solution {
     public int searchInsert(int[] a, int t) {
         int l = 0, r = a.length - 1;
